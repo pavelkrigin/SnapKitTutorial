0c72a481aaf813d2c943b1043de97b814bcaae36
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-05-06 15:18:26 +0200

    startCommit

--- SnapKitTutorial/ViewController.swift
+++ SnapKitTutorial/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let button = UIButton(type: .system)
+    let textField = UITextField()
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupButton()
+        setupTextFiel()
+        setupLabel()
     }
-
-
+    
+    //MARK: layouts methods
+    func setupButton() {
+        view.addSubview(button)
+        button.backgroundColor = .green
+        button.setTitle("PUSH", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.layer.cornerCurve = CALayerCornerCurve.continuous
+        button.layer.cornerRadius = 20
+        button.snp.makeConstraints { button in
+            button.width.equalTo(110)
+            button.height.equalTo(55)
+            button.center.equalTo(view)
+        }
+    }
+    
+    func setupTextFiel() {
+        view.addSubview(textField)
+        textField.backgroundColor = .lightGray
+        textField.layer.cornerRadius = 7
+        textField.snp.makeConstraints { textField in
+            textField.width.equalTo(150)
+            textField.height.equalTo(36)
+            textField.bottom.equalTo(button.snp.top).inset(-60)
+            textField.centerX.equalTo(view)
+        }
+    }
+    
+    func setupLabel() {
+        view.addSubview(label)
+        label.text = ""
+        label.textAlignment = .center
+        label.snp.makeConstraints { label in
+            label.width.equalTo(200)
+            label.height.equalTo(150)
+            label.bottom.equalTo(textField.snp.top).inset(-30)
+            label.centerX.equalTo(view)
+        }
+    }
+    
+    @objc func buttonAction() {
+        label.text = textField.text
+    }
+    
 }
 
