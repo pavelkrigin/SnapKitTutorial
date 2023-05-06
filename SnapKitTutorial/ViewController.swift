e80cb76abe6bc979306ab73367a1dd0831b3703f
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-05-06 14:08:45 +0200

    Initial Commit

---
+++ SnapKitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitTutorial
+//
+//  Created by Pavel Krigin on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
