67a9fc02cdf0e0d7f54e591375292e4eced285aa
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-05-07 23:16:16 +0200

    everyDayWorkCommit

--- SnapKitTutorial/ViewController.swift
+++ SnapKitTutorial/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     let button = UIButton(type: .system)
     let textField = UITextField()
